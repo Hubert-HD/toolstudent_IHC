dc60eaac775557c415ceb04a2da48bf57a77e001
AlexRTC <aturpoco@unsa.edu.pe>
2020-12-12 18:25:24 -0500

    Preparacion para subir a Github Pages

---
+++ docs/precache-manifest.defde6a644ea567aea67e5acdb49c652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "defffa9510b1795379bd17ea59c5a701",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ba0e6a6f08190aeed91",
+    "url": "/static/css/main.217e7527.chunk.css"
+  },
+  {
+    "revision": "999a468761bc6409bae6",
+    "url": "/static/js/2.ba79c5d0.chunk.js"
+  },
+  {
+    "revision": "09443f2c1df71aee499e73478b2dd8c3",
+    "url": "/static/js/2.ba79c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ba0e6a6f08190aeed91",
+    "url": "/static/js/main.4028a776.chunk.js"
+  },
+  {
+    "revision": "74d333d932f437098253",
+    "url": "/static/js/runtime-main.046e65b7.js"
+  }
+]);
