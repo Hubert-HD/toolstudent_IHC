e8dee0e6b2a23ee855dc799223c128065acd12aa
AlexRTC <aturpoco@unsa.edu.pe>
2020-12-12 17:10:36 -0500

    Creacion de Grafica De Tipo Radar

--- src/components/pages/ChartsPage.jsx
+++ src/components/pages/ChartsPage.jsx
@@ -136,22 +136,12 @@ const ChartsPage = () => (
                     labels: ['IHC', 'EMAT', 'MIR', 'ADA', 'EE', 'AC'],
                     datasets: [{
                         label: 'Notas finales del semestre 2020 B',
-                        data: [12, 19, 3, 5, 2, 3],
+                        data: [15, 17, 19, 13, 16, 11],
                         backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
                             'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
                         ],
                         borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
                             'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
                         ],
                         borderWidth: 1
                     }],
@@ -161,6 +151,15 @@ const ChartsPage = () => (
                 width={600}
                 options={{
                     maintainAspectRatio: false,
+                    scale: {
+                        angleLines: {
+                            display: false
+                        },
+                        ticks: {
+                            suggestedMin: 0,
+                            suggestedMax: 20
+                        }
+                    }
                 }}
             />
         </div>
